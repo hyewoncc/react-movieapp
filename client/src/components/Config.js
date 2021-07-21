62dfe49dd3466be7b8417a20e1e9b1f5ddb24a25
hyewoncc <hyewoncc@gmail.com>
2021-07-22 00:55:04 +0900

    ADD 서버 경로 설정 / 서버 포트 변경

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -4,3 +4,4 @@ export const USER_SERVER = '/api/users';
 export const API_URL = 'http://api.themoviedb.org/3/';
 
 export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+export const API_KEY = '24b625a2a75f12fa3a570e89c9149c6c';
