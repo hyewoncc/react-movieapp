9be94b133285bafa3059ce2091b1a80d1cce1900
hyewoncc <hyewoncc@gmail.com>
2021-08-10 16:26:18 +0900

    ADD 배우의 출연 영화를 GridCards로 보여주고 링크 추가 배우 상세보기에서 출연 영화를 GridCards로 포스터 보여줌 클릭 시 영화 상세페이지로 이동

--- client/src/components/views/ActorDetail/ActorDetail.js
+++ client/src/components/views/ActorDetail/ActorDetail.js
@@ -1,21 +1,31 @@
 import React, { useEffect, useState } from 'react'
 import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
 import ActorInfo from './ActorInfo';
+import GridCards from '../commons/GridCards';
+import { Row } from 'antd';
+
 
 function ActorDetail(props) {
 
     let actorId = props.match.params.actorId 
     const [Actor, setActor] = useState([])
-    
+    const [Credits, setCredits] = useState([])
 
     useEffect(() => {
 
         let endpointActor = `${API_URL}person/${actorId}?api_key=${API_KEY}`
+        let endpointCredits = `${API_URL}person/${actorId}/movie_credits?api_key=${API_KEY}`
 
         fetch(endpointActor)
             .then(response => response.json())
             .then(response => {
                 setActor(response)
+            })
+        
+        fetch(endpointCredits)
+            .then(response => response.json())
+            .then(response => {
+                setCredits(response)
             })
 
     }, [])
@@ -26,14 +36,33 @@ function ActorDetail(props) {
             {/* Body */}
             <div style={{ width: '85%', margin: '1rem auto' }}>
 
-                {/* Actor Info */}
-
-                <ActorInfo
-                    actor = { Actor }
-                />
+                {/* Actor Picture and Info*/}
+                <div style={{ display: 'flex'}}>
+                    <div>
+                        <img style={{ width: '300px', objectFit: 'contain'}} src={`${IMAGE_BASE_URL}w500${Actor.profile_path}`} alt={Actor.name}/>
+                    </div>
+                    <div style={{ marginLeft: '10px' }}>
+                        <ActorInfo
+                            actor = { Actor }
+                        />
+                    </div>
+                </div>
 
                 <br/>
                 {/* Movie Credits Grid */}
+                <Row gutter={[16, 16]}>
+                    {Credits.cast && Credits.cast.map((movie, index) => (
+                        <React.Fragment key={index}>
+                            <GridCards 
+                                landingPage
+                                image={movie.poster_path ?
+                                    `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
+                                movieId={movie.id}
+                                movieName={movie.original_title}
+                            />
+                        </React.Fragment>
+                    ))}
+                </Row>
 
             </div>
         
--- client/src/components/views/commons/GridCards.js
+++ client/src/components/views/commons/GridCards.js
@@ -18,7 +18,7 @@ function GridCards(props) {
             <Col lg={6} md={8} xs={24}>
                 <div style={{ position: 'relative'}}>
                     <a href={`/actor/${props.personId}`}>
-                        <img style={{ width: '100%', height: '320px'}} src={props.image} alt={props.characterName}/>
+                        <img style={{ width: '100%', height: '100%'}} src={props.image} alt={props.characterName}/>
                     </a>
                 </div>
             </Col>
